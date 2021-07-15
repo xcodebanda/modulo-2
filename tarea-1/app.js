388a4ca0f620c45d67e7045d5b320ecacc5cae52
xcodebanda <xcode.banda@gmail.com>
2021-07-15 19:57:14 -0300

    tarea 1 - ejercico 2

--- tarea-1/app.js
+++ tarea-1/app.js
@@ -1,5 +1,17 @@
+const path = require('path')
+const fs = require('fs');
+
+let text = ''
+
 for (let i = 1; i <= 10; i++) {
-    console.log(
-        `5 x ${i} = ${5*i}`
-    )
+    text += `5 x ${i} = ${5*i}\n`
 }
+
+const fileName = 'tabla-5.txt'
+fs.writeFile(path.join(__dirname, fileName), text, (error) => {
+    if (error) {
+        console.log('Hubo un problema!')
+        return
+    }
+    console.log('Tarea completada!')
+})
